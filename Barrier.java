9a282524571e6708386d2ad6485ea0a274613dbc
Laptop User <laptop@loanlaptop.cs.uct.ac.za>
2019-04-18 02:35:31 +0200

    Finished the b_wait() method of the Barrier class

--- Barrier.java
+++ Barrier.java
@@ -3,30 +3,30 @@ import java.util.concurrent.Semaphore;
 
 public class Barrier {
 	
-	// add missing variables
-	int counter;
 	Semaphore sem;
-	
-	Barrier(int n) {
-		// complete this constructor
-		counter = n;
-		size = n;
-		released = true;
-		sem = new Semaphore(n); // n threads can get in the critical section
+   Semaphore mutex;
+   int barrierSize;
+   int numThreads;
+   
+	Barrier( int n ) {
+      sem = new Semaphore( 0 ); 
+      barrierSize = n;              // n is the size of the barrier 
+      mutex = new Semaphore( 1 );   // allowing only one thread to execute the critical section
+      numThreads = 0;               // No threads arrived yet
 	}
 	
+   // Every thread will have a copy of this method
 	public void b_wait() throws InterruptedException{
-		// this is the only additional method you will need to complete
-
-		sem.acquire();
 		
-		if( counter > 0){
-			counter -= 1; // decrement the counter
-			while(counter > 0){
-				/* Make the thread wait until all threads arrive. */
-			}
-		}
-		sem.release();;
+      mutex.acquire();   // acquire the lock
+      numThreads++;      // read, update, and store counter in memory
+      mutex.release();   // release the lock for the otyher thread to update the counter
+
+      if( numThreads < barrierSize ){ // reading from memory i.e no update on counter
+         
+         sem.acquire();
+      }
+      sem.release();
 	}
 
 }
